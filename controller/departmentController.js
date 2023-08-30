7017946df4d38c23677a09fe22b69d044502b942
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-08-30 08:09:28 +0300

    fixing the express handler ' '

--- controller/departmentController.js
+++ controller/departmentController.js
@@ -1,4 +1,4 @@
-const asyncHandler=require(express-async-handler);
+const asyncHandler=require('express-async-handler');
 //@disc create new department 
 //@routes POST /api/departments/
 //@access Public
