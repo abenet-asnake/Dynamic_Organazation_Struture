4ceab9f66ea34534af7b4cc67b13a55242d9bd59
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-08-30 08:06:08 +0300

    Installing express-async-handler wrap up all controller function

--- controller/departmentController.js
+++ controller/departmentController.js
@@ -1,36 +1,37 @@
+const asyncHandler=require(express-async-handler);
 //@disc create new department 
 //@routes POST /api/departments/
 //@access Public
-const createDepartments =(req,res)=>{
+const createDepartments = asyncHandler(async (req,res)=>{
     res.status(200).json({message: 'New departments'});
-}
+});
 //@disc get all departments 
 //@routes GET /api/departments
 //@access Public
-const getDepartments =(req,res)=>{
+const getDepartments = asyncHandler(async(req,res)=>{
     res.status(200).json({message: 'Get all departments'});
-}
+});
 
 //@disc get department by id 
 //@routes GET /api/departments/:id
 //@access Public
-const getDepartmentsByID =(req,res)=>{
+const getDepartmentsByID = asyncHandler(async(req,res)=>{
     res.status(200).json({message: `Get Department by ${req.params.id}`});
-}
+});
 
 //@disc Edit department by id 
 //@routes PUT /api/departments/:id
 //@access Public
-const editDepartmentsByID =(req,res)=>{
+const editDepartmentsByID = asyncHandler(async(req,res)=>{
     res.status(200).json({message:`Update department by ${req.params.id}`});
-};
+});
 
 //@disc delete department by id
 //@routes DELETE /api/departments/:id
 //@access Public
-const deleteDepartmentsByID =(req,res)=>{
+const deleteDepartmentsByID = asyncHandler(async(req,res)=>{
     res.status(200).json({message:`Delete department by ${req.params.id}`});
-};
+});
 
 module.exports = {createDepartments,
                   getDepartments,
